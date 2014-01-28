777b98bbfefd417abcbc2c728dcc0f798948cbe5
Syfaro <syfaro@me.com>
2014-01-28 17:45:22 -0600

    basically a complete overhaul

--- guestbook.py
+++ guestbook.py
@@ -1,53 +1,125 @@
+import os
 import datetime
+import sqlite3
 
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, g
 
 
 app = Flask(__name__)
-entries = []
-entry_names = []
-entry_dates = []
+DATABASE = os.getcwd() + 'guestbook.py'
+
+
+def get_database():
+    return sqlite3.connect(DATABASE)
+
+
+def get_entries():
+    db = g.db
+
+    c = db.cursor()
+    c.execute(
+        'SELECT `id`, `name`, `message`, `posted_at` FROM `messages` \
+            ORDER BY `id` DESC')
+    entries = c.fetchall()
+
+    return entries
+
+
+def add_entry(name, message):
+    db = g.db
+
+    c = db.cursor()
+    c.execute('INSERT INTO `messages` (`name`, `message`) VALUES (?, ?)',
+              (name, message, ))
+
+    db.commit()
+
+
+def remove_entry(id):
+    db = g.db
+
+    c = db.cursor()
+    c.execute('DELETE FROM `messages` WHERE `id` = ?', id)
+
+    db.commit()
+
+
+@app.before_request
+def before_request():
+    if getattr(g, 'db', None) is None:
+        g.db = get_database()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+
+    if db is not None:
+        db.close()
 
 
 @app.route('/')
 def index():
     return render_template(
-        'index.html', entries=zip(entry_names, entry_dates, entries))
+        'index.html', entries=get_entries())
+
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    entries.append(request.form['entry'] or '<blank>')
     if not request.form['name']:
-        flash("you must provide a name!")
+        flash('You must provide a name!')
         return redirect('/')
-    entry_names.append(request.form['name'])
-    entry_dates.append(datetime.datetime.now())
+
+    add_entry(request.form['name'], request.form['entry'])
+
+    flash('Message added!')
     return redirect('/')
+
 
 @app.route('/admin')
 def admin_login():
+    if 'logged_in' in session and session['logged_in']:
+        return redirect('/')
+
     return render_template('login.html')
+
 
 @app.route('/admin', methods=['POST'])
 def do_admin_login():
     if request.form['password'] == 'password':
         session['logged_in'] = True
+        return redirect('/')
+
     else:
-        flash('wrong password!')
+        flash('Wrong password!')
+
     return admin_login()
+
 
 @app.route('/moderate', methods=['POST'])
 def moderate_posts():
-    for key in request.form:
-        if not key.startswith('delete_'):
-            continue
-        index = int(key.partition('_')[2])
-        del entries[index]
-        del entry_names[index]
-    return redirect('/')
+    if session['logged_in']:
+        remove_entry(request.form['id'])
+
+        flash('Removed entry')
+        return redirect('/')
+
+    flash('You need to be logged in to do this!')
+    return redirect('/admin')
 
 
 if __name__ == '__main__':
-    import os
+    db = get_database()
+
+    c = db.cursor()
+    c.execute(
+        'CREATE TABLE IF NOT EXISTS `messages` ( \
+            id INTEGER PRIMARY KEY, \
+            name VARCHAR(255), \
+            message TEXT, \
+            posted_at DATETIME DEFAULT CURRENT_TIMESTAMP)')
+
+    db.close()
+
     app.secret_key = os.urandom(12)
     app.run(debug=True)
